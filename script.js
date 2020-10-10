f77e0db9f3e44a5a666c75f2c3bd1edb504336c9
Rafael Morais <rafahgmorais@gmail.com>
2020-10-10 18:38:49 -0300

    panel da direita, rodapé

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ setInterval(() => {
   display.style.transition = "all 2s";
   display.style.opacity = "0";
 
-  console.log(currentIndex, nextIndex);
   setTimeout(() => {
     display.style.backgroundImage = `url("./${images[nextIndex]}")`;
 
@@ -35,3 +34,42 @@ setInterval(() => {
     buffer.style.backgroundImage = `url("./${images[nextIndex]}")`;
   }, 2500);
 }, 3000);
+
+const togglePasswordButton = document.querySelector("#toggleShowPassword");
+const passwordInput = document.querySelector("#password");
+
+togglePasswordButton.addEventListener("click", () => {
+  const type =
+    passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+
+  if (type === "password") {
+    togglePasswordButton.innerHTML = "Mostrar";
+  } else {
+    togglePasswordButton.innerHTML = "Ocultar";
+  }
+});
+
+const siginButton = document.querySelector("#signinButton");
+const userInput = document.querySelector("#userInput");
+
+userInput.addEventListener("input", (e) => {
+  if (passwordInput.value.length >= 6 && e.target.value.length > 0) {
+    siginButton.className = "signin-button-enabled";
+  } else {
+    siginButton.className = "signin-button";
+  }
+});
+
+passwordInput.addEventListener("input", (e) => {
+  if (e.target.value.length > 0) {
+    togglePasswordButton.style.display = "block";
+  } else {
+    togglePasswordButton.style.display = "none";
+  }
+  if (e.target.value.length >= 6 && userInput.value.length > 0) {
+    siginButton.className = "signin-button-enabled";
+  } else {
+    siginButton.className = "signin-button";
+  }
+});
